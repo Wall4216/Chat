3629fb0294ac823f65a298dda4f9b8471941f69e
Wall <ilnazamirhanov61@gmail.com>
2023-01-07 02:58:20 +0300

    add ChatController

--- laravel/app/Http/Controllers/ChatController.php
+++ laravel/app/Http/Controllers/ChatController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\MessageSent;
+use App\Http\Requests\MessageFormRequest;
+use App\Models\Message;
+use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
-    //
+    public function index(): Factory|View|Application
+    {
+        auth()->loginUsingId(1);
+
+        return view('chat');
+    }
+
+    public function message()
+    {
+        return Message::query()
+            ->with('user')
+            ->get();
+    }
+    public function send(MessageFormRequest $request)
+    {
+        $message = $request->user()
+            ->messages()
+            ->create($request->validate());
+        broadcast(new MessageSent($request->user(), $message));
+        return $message;
+    }
 }
