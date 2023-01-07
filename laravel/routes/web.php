1aa6b9f4f90a01d31335dcdd6c58cc124272b8b7
Wall <ilnazamirhanov61@gmail.com>
2023-01-07 03:02:58 +0300

    add 2 app:routes

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
+
 Route::controller(ChatController::class)->group(function(){
     Route::get('/', 'index');
     Route::get('/messages', 'messages');
